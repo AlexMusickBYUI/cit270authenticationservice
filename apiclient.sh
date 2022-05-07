65ade1cbf2226f5751081902f3dd791b7676709a
AlexMusickBYUI <mus19002@byui.edu>
2022-05-07 13:18:31 -0600

    Authentication service functional

--- apiclient.sh
+++ apiclient.sh
@@ -1,1 +1,1 @@
-curl -d "@data.json" -X POST http://localhost:3000/login
+curl -d "@data.json" -X POST http://localhost:3000/login -H "content-type:application/json"
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser'); //body-parser is middleware
 const port = 3000;
 const app = express(); // "App" constant = "application" method of express library (in this case that basically means to actuallly use the library)
 
-app.use(bodyParser.json()); // tell express to use bodyParser (call it before anything else happens on each request)
+app.use(bodyParser.json()); // tell express to use bodyParser.json() (call it before anything else happens on each request)
 
 app.listen(port, ()=>{
     console.log("listening... on port: "+port);
@@ -19,11 +19,13 @@ app.post('/login',(request,response)=>{
     const loginRequest = request.body;
     if (loginRequest.userName=='notreal_bob_jones@ams.net' && loginRequest.password=='aA1!aa'){
         response.status(200);
-        console.log(loginRequest.userName);
+        // console.log(loginRequest.userName);
         response.send('Welcome');
     } else {
         response.status(401);
         response.send('Unauthorized');
-        console.log(loginRequest.userName);
+        // console.log(loginRequest.userName);
     }
 });
+//This works the way it looks like it should, but the issue when it didn't work was that the header was missing.
+//For JSON, body-parser expects the header "content-type:application/json". In curl, you can add a header using -H "[header]".
